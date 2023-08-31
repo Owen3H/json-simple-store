cca14e4411a3c3ccebdda26e9450a49951d44148
O.S <owen77stubbs@gmail.com>
2023-08-31 12:17:59 +0100

    convert to typescript

--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,26 @@
-const fs = require('fs'),
-      { constants, promises: pfs } = fs
+import { 
+    PathLike,
+    constants, 
+    promises as pfs 
+} from 'fs'
 
-module.exports = class JSONStore {
-    #defaultPath = '/default_store.json'
-    relativePath = ''
-    absolutePath = ''
-
+export default class JSONStore {
     #data = {}
 
-    constructor(path=this.#defaultPath) {
-        const firstChar = path.charAt(0)
+    #defaultPath = '/default_store.json'
+    private relativePath = ''
+    private absolutePath = ''
 
+    constructor(path: string) {
+        if (!path) path = this.#defaultPath
+
+        const firstChar = path.charAt(0)
         if (firstChar === '.') path = path.slice(1)
         if (!path.includes('/')) path = `/` + path
 
         this.relativePath = path
+        this.absolutePath = process.cwd() + path
+
         this.#sync()
     }
 
@@ -49,23 +55,30 @@ module.exports = class JSONStore {
         return curPath
     }
 
-    async get(key) { 
-        return this.#sync().then(data => data[key]) 
-    }
+    get = (key: string) => this.#sync().then(data => data[key]) 
 
-    async set(key, value)  {
+    async set(key: string, value: any)  {
         this.#data[key] = value
         await this.#write(this.#data)
     }
 
-    add(key, items) { this.#data[key].push(items) }
-    async empty() { await this.#write({}) }
+    add(key: string, items) { this.#data[key].push(items) }
+    empty = () => this.#write({})
 
-    async exists(path) {
-        // No error = accessed successfully
-        return pfs.access(path, constants.F_OK).then(() => true).catch(() => false)
+    exists = async (path: PathLike) => {
+        // If no error, it was accessed successfully.
+        try {
+            return !!pfs.access(path, constants.F_OK)
+        } catch(e) {
+            return false
+        }
     }
 
-    #read = path => this.exists(path) ? pfs.readFile(path).catch(() => false) : false
-    #write = async (data, path=this.absolutePath) => pfs.writeFile(path, JSON.stringify(data, null, '\t')).catch(() => false)
+    #read = (path: PathLike) => this.exists(path) ? pfs.readFile(path).catch(() => false) : false
+    #write = async (data, path?: PathLike) => {
+        if (!path) path = this.absolutePath
+
+        try { return pfs.writeFile(path, JSON.stringify(data, null, '\t')) }
+        catch(e) { return false }
+    }
 }
